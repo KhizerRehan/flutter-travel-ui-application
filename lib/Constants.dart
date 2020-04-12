f91dc2a007955f57dc9b3397619f67f0f9ac1bd4
Khizer Rehan <khizer.rehan@aurorasolutions.io>
2020-04-12 18:16:43 +0500

    use seperate constant file instead of inline hardcoding values

---
+++ lib/Constants.dart
@@ -0,0 +1,17 @@
+class Constants {
+  int _primaryColor = 0xFF3EBACE;
+  int _accentColor = 0xFFD8ECF1;
+  int _scaffoldBackgroundColor = 0xFFE3F5F7;
+
+  get primaryColor {
+    return this._primaryColor;
+  }
+
+  get accentColor {
+    return this._accentColor;
+  }
+
+  get scaffoldBackgroundColor {
+    return this._scaffoldBackgroundColor;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_travel_ui_starter/Constants.dart';
 import 'package:flutter_travel_ui_starter/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  Constants constant = new Constants();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Travel UI',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: Color(0xFF3EBACE),
-        accentColor: Color(0xFFD8ECF1),
-        scaffoldBackgroundColor: Color(0xFFF3F5F7),
+        primaryColor: Color(constant.primaryColor),
+        accentColor: Color(constant.accentColor),
+        scaffoldBackgroundColor: Color(constant.scaffoldBackgroundColor),
       ),
       home: HomeScreen(),
     );

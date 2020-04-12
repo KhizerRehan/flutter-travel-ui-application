ced2001c60a407bdbe94601c8873eee251c4195a
Khizer Rehan <khizer.rehan@aurorasolutions.io>
2020-04-12 22:09:52 +0500

    add top icons in screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_travel_ui_starter/Constants.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,6 +8,40 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int selectedIconIndex = 0;
+  List<IconData> _icons = [
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.walking,
+    FontAwesomeIcons.biking,
+  ];
+
+  Widget _buildIcon(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedIconIndex = index;
+        });
+        print('Selected Icon Index is:$selectedIconIndex');
+      },
+      child: Container(
+        height: 65.0,
+        width: 65.0,
+        decoration: BoxDecoration(
+          color: selectedIconIndex == index
+              ? Theme.of(context).accentColor
+              : Color(Constants.selectedAccentColor),
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        child: Icon(_icons[index],
+            size: 28.0,
+            color: selectedIconIndex == index
+                ? Theme.of(context).primaryColor
+                : Color(Constants.selectedIconColor)),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,17 +52,31 @@ class _HomeScreenState extends State<HomeScreen> {
             Padding(
               padding: const EdgeInsets.only(
                 left: 20.0,
-                right: 120.0,
+                right: 140.0,
               ),
               child: Text(
                 'What would you like to find?',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
-                  fontSize: 30.0,
+                  fontSize: 22.0,
+                  letterSpacing: 1.0,
+                  height: 1.3,
                 ),
               ),
             ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: _icons
+                    .asMap()
+                    .entries
+                    .map(
+                      (MapEntry map) => _buildIcon(map.key),
+                    )
+                    .toList())
           ],
         ),
       ),
